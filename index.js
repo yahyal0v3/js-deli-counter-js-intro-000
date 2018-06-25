bdf766940a24de827bd326453848fe6209ce4b7d
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-06-25 19:33:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
-  return 'Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.'
+  return `Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.`
 }
 
 function nowServing() {
