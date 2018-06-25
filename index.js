75d97d5b4797bb8c930471b9acad1726d0c52a32
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-06-25 20:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(line, name) {
 }
 
 function nowServing(line) {
-  if (line === []) {
+  if (line.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
     return `Currently serving ${line.shift()}.`
